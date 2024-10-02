ebe01d28446c2be2d40b56355d8be09e61ac3f8c
João Pires <joaopires51@outlook.com>
2024-10-02 17:00:30 +0100

    add licence

--- code_emission.cpp
+++ code_emission.cpp
@@ -1,3 +1,22 @@
+/*
+ * Will Compile C for Food, a toy C compiler
+ * Copyright (C) 2024  João Pires
+ * https://github.com/jpires/will-compile-c-for-food
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #include "code_emission.h"
 #include "visitor.h"
 #include <fstream>
